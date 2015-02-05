f6a49850c941b2657ff890d645101e28f36867f0
amit-mittal <amit.mittal1993@gmail.com>
2015-02-05 13:34:56 +0530

    Recursive partitioning sum...done

--- sum_array_recursive_part.c
+++ sum_array_recursive_part.c
@@ -5,23 +5,24 @@
 
 int main(int argc, char *argv[]){
 	int myid, numprocs;
-	int count, source, i, j, tag, left_sum, right_sum;
+	int count, source, i, j, tag;
+	long long int left_sum, right_sum;
 	int *buffer, *temp;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
 	MPI_Comm_rank(MPI_COMM_WORLD, &myid);
 
-	count = 10000;
+	count = 500000;
 	source = 1;
 	tag = 1234;
 
 	int total = count*(numprocs/2);
 	buffer = (int*)malloc(total*sizeof(int));
 	
-	int left = 2*myid;
-	int right = left+1;
-	int parent = myid/2;
+	int left = 2*myid; // left child
+	int right = left+1; // right child
+	int parent = myid/2; // parent node
 	bool if_source = (source == myid);
 	bool if_leaf = (right >= numprocs);
 
@@ -29,49 +30,56 @@ int main(int argc, char *argv[]){
 	{
 		if (myid == source)
 		{
+			// making the data in the source
 			for (i = 0; i < total; ++i)
 				buffer[i] = i;
 		}
 		else
 		{
+			// if not source receiving the no. of 
 			MPI_Recv(&total, 1, MPI_INT, parent, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 			MPI_Recv(buffer, total, MPI_INT, parent, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);	
 		}
 
 		if(!if_leaf)
 		{
+			// Sending buffer size and data to left child
 			int val = total/2;
 			MPI_Send(&val, 1, MPI_INT, left, tag, MPI_COMM_WORLD);
 			MPI_Send(buffer, total/2, MPI_INT, left, tag, MPI_COMM_WORLD);
 
+			// Sending buffer size and data to right child
 			MPI_Send(&val, 1, MPI_INT, right, tag, MPI_COMM_WORLD);
 			MPI_Send(buffer+(total/2), total/2, MPI_INT, right, tag, MPI_COMM_WORLD);
 
-			MPI_Recv(&left_sum, 1, MPI_INT, left, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-			MPI_Recv(&right_sum, 1, MPI_INT, right, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			// Receiving the respective left and right sum
+			MPI_Recv(&left_sum, 1, MPI_LONG_LONG_INT, left, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			MPI_Recv(&right_sum, 1, MPI_LONG_LONG_INT, right, tag, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-			int ans = left_sum + right_sum;
+			long long int ans = left_sum + right_sum;
 			if(myid == source)
 			{
-				printf("Final sum is %d\n", ans);			
+				printf("Final sum is %lld\n", ans);			
 			}
 			else
 			{
-				MPI_Send(&ans, 1, MPI_INT, parent, tag, MPI_COMM_WORLD);
+				// Sending back the sum computed
+				MPI_Send(&ans, 1, MPI_LONG_LONG_INT, parent, tag, MPI_COMM_WORLD);
 			}
 		}
 		else
 		{
-			int sum = 0;
+			// computing the sum in leaf node
+			long long int sum = 0LL;
 			for (i = 0; i < total; ++i)
 			{
 				sum+=buffer[i];
 			}
 
-			MPI_Send(&sum, 1, MPI_INT, parent, tag, MPI_COMM_WORLD);
+			// Sending sum back to parent
+			MPI_Send(&sum, 1, MPI_LONG_LONG_INT, parent, tag, MPI_COMM_WORLD);
 		}
-	}
-	
+	}	
 
 	MPI_Finalize();
 
